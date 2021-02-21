92d0e83721c1a5485593b36f1660e3369dbb0405
Camilo Lopes <Camilo_m_lopes@hotmail.com>
2021-02-21 10:36:51 -0300

    rota de função funcionando

--- src/Conversor/Conversor.js
+++ src/Conversor/Conversor.js
@@ -1,8 +1,46 @@
-import React from 'react';
+import React,{useState}from 'react';
 import './Conversor.css'
 import Buttons from './Buttons/Index'
 
 export default function Conversor() {
+  
+  const [data,setData] = useState("");
+  var butTo = ('') 
+  var butFor = ('')
+
+  function Verify(props){
+    if(butTo.value === 'Json' && butFor.value === 'Xml'){
+      JsonForXml();
+    }
+    if(butTo.value === 'Json' && butFor.value === 'Csv'){
+      alert('Formato Csv ainda não implementado')
+    }
+    if(butTo.value === 'Xml' && butFor.value === 'Csv'){
+      alert('Formato Csv ainda não implementado')
+    }
+    if(butTo.value === 'Xml' && butFor.value === 'Json'){
+      XmlForJson();
+    }
+    if(butTo.value === 'Csv' && butFor.value === 'Xml'){
+      alert('Formato Csv ainda não implementado')
+    }
+    if(butTo.value === 'Csv' && butFor.value === 'Json'){
+      alert('Formato Csv ainda não implementado')
+    }
+    if(butTo.value === butFor.value){
+      alert('Conversão em formatos iguais')
+    }
+  }
+  
+  function JsonForXml(){
+
+  }
+  function XmlForJson(){
+
+  }
+
+
+
   return (
    <React.Fragment>
     <div className="header">
@@ -12,17 +50,20 @@ export default function Conversor() {
       <div className="prev-value-container">
         <h4 className="to">To</h4>
         <div className="prev-value">
-          <Buttons />
+          <Buttons onClickVerify={(event)=>{butTo =event}}/>
         </div>
       </div>
-      <textarea className ="value" name="value" id="value" cols="30" rows="10">
+      <textarea className ="value" name="value" value={data}
+       cols='30' rows='10' onChange={(event)=>setData(event.target.value)}>
+      {data}
       </textarea>
       <div className="next-value-container">
         <h4 className="for">For</h4>
         <div className="next-value">
-          <Buttons />
+          <Buttons onClickVerify= {(event)=>{butFor =event}}/>
         </div>
       </div>
+      <input onClick={()=>Verify()} type="button" className='btn-convert' value="Coverter"/>
     </div>
    </React.Fragment>
   );
